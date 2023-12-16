9c76d138789ab73681a836f01009d56d69ff3532
mertSezerO <mertS.Oktay@gmail.com>
2023-12-16 22:02:45 +0300

    Button component implemented.

---
+++ frontend/src/components/button.js
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Button(name, redirectPath) {
+    const navigate = useNavigate()
+
+    function onClick() {
+        navigate(redirectPath)
+    }
+
+    return (
+        <button type="button" onClick={onClick}>{name}</button>
+    )
+}
